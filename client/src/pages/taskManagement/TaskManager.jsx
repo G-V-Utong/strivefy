c62ec18764fbfbf5a290a57879b181f6567f7464
Godswill Utong <godswillutong@gmail.com>
2023-12-20 00:00:22 +0100

    task list component created

--- client/src/pages/taskManagement/TaskManager.jsx
+++ client/src/pages/taskManagement/TaskManager.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./taskManager.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
+import AddTask from "../../components/taskManager/AddTask";
+import TaskList from "../../components/taskManager/TaskList";
 
 const TaskManager = () => {
   return (
     <div className="taskmanager">
       <div className="taskmanager__left">
         <Sidebar />
+      </div>
+      <div className="taskmanager__right">
+        <AddTask/>
+        <TaskList/>
       </div>
     </div>
   );
