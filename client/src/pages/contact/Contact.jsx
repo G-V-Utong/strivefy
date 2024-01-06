af577f9166801902170b0b1d3858842eed21448c
Godswill Utong <godswillutong@gmail.com>
2024-01-06 13:55:31 +0100

    contact us page UI updated

--- client/src/pages/contact/Contact.jsx
+++ client/src/pages/contact/Contact.jsx
@@ -1,3 +1,8 @@
+/* eslint-disable react/no-unescaped-entities */
+import Footer from '../../components/footer/Footer';
+import { Link } from "react-router-dom";
+import { FaGithub, FaInstagramSquare, FaLinkedin } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 import './contact.scss'
 
 const Contact = () => {
@@ -5,57 +10,36 @@ const Contact = () => {
     <div className="about">
       <div className="about__container"></div>
       <div className="about__hero">
-        <h1>About us</h1>
+        <h1>Contact us</h1>
         <img
           src="/public/images/contact.jpg"
           alt="contact picture"
           width={500}
         />
         <div className="about__hero__text">
-          <h2>Team Overview</h2>
-          <h3>Founder and Developer – Godswill Utong</h3>
+          <p>For general enquiries please email</p>
+          <h3>godswillutong@gmail.com</h3>
           <p>
-            As the founder and sole developer behind Strivefy, Godswill Utong
-            brings a wealth of experience and expertise to the forefront of task
-            management app development. With a Bachelor's degree in Computer
-            Engineering from Bayero University Kano and over seven years of
-            professional experience, Godswill has a proven track record in both
-            the financial sector, where they served as a Relationship Manager
-            with Wema Bank and Access Bank Plc, and in the tech industry.
+            You can reach us on phone via
           </p>
-          <h3>Key Skills</h3>
+          <h3>+234-816-983-0193</h3>
           <p>
-            Full-stack development with a focus on web and mobile applications.
-            Proficient in NextJs, ReactJs, and Redux for frontend development.
-            Extensive experience in building and maintaining robust and
-            user-friendly applications. Design and implementation of rigorous
-            testing suites to ensure optimal performance. A keen eye for
-            identifying and resolving design and implementation bugs.
+            Keep in touch with us through our social media platforms
           </p>
-          <h3>Vision and Mission</h3>
-          <p>
-            Godswill Utong's vision for Strivefy is rooted in a commitment to
-            providing users with a seamless and efficient task management
-            experience. The mission is to create a tool that not only meets but
-            anticipates the dynamic needs of users, enhancing productivity and
-            simplifying daily workflows.
-          </p>
-          <h3>User-Centric Approach</h3>
-          <p>
-            As the sole member of the team, Godswill places a high emphasis on
-            user feedback, actively incorporating suggestions and insights into
-            continuous improvements. This user-centric philosophy drives the
-            ongoing development and evolution of Strivefy.
-          </p>
-          <h3>Future Development</h3>
-          <p>
-            With an unwavering dedication to innovation, he envisions Strivefy
-            as a continuously evolving platform. Future plans include the
-            implementation of new features and technologies to keep pace with
-            the ever-changing landscape of task management. Strivefy is not just
-            an app; it's a reflection of Godswill's passion for creating
-            solutions that empower users to strive for their best.
-          </p>
+          <div className="social_media">
+          <Link to="https://github.com/G-V-Utong">
+              <FaGithub size={30} />
+            </Link>
+            <Link to="https://www.instagram.com/g.v.utong/">
+              <FaInstagramSquare size={30} />
+            </Link>
+            <Link to="https://twitter.com/G_VestaUtong">
+              <FaXTwitter size={30} />
+            </Link>
+            <Link to="https://www.linkedin.com/in/godswill-utong-10252915b/">
+              <FaLinkedin size={30} />
+            </Link>
+          </div>
         </div>
       </div>
         
